df3d7354d390940dcd230e8b20d9687e6792e0e4
Surabhi Gupta <surabhi.simpi412@gmail.com>
2017-08-06 02:17:40 +0530

    added tcp server and client programs in c and java programs respectively, added simulation of mouse and keyboard events in os x m/c

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # android-remote-mouse
 An android application which works as a remote mouse to OS X machine.
+
+## To run TCP server
+
+* cd RemoteDesktopServer
+* `gcc -o tcp tcp_server.c`
+* `./tcp <port_number>`
+
+## To run TCP client
+
+* cd AndroidRemoteClient
+* `javac TCPClient.java`
+* `java TCPClient <host_address> <port_number>`
---
+++ RemoteDesktopServer/tcp_server.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define BUFSIZE 1024
+
+int main(int argc, char **argv) {
+  int port_number, parentfd, childfd, optval, n;
+  struct sockaddr_in serveraddr, clientaddr;
+  struct hostent *hostp; /* client host(DNS) info */
+  char buf[BUFSIZE];
+  char output_msg[BUFSIZE] = "Thank you for connecting with our server"; /* message buffer */
+  char *hostaddrp; /* dotted decimal host addr string */
+  socklen_t clientlen;
+
+  if(argc != 2) {
+    fprintf(stderr, "Please provide port number");
+    exit(1);
+  }
+  port_number = atoi(argv[1]);
+
+  //Creating socket
+  parentfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (parentfd < 0) {
+    fprintf(stderr, "ERROR opening socket");
+    exit(1);
+  }
+
+  /* setsockopt: Handy debugging trick that lets us rerun the server immediately after we kill it otherwise we have to wait about 20 secs.
+   * Eliminates "ERROR on binding: Address already in use" error.
+   */
+  optval = 1;
+  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+
+  // Building server socket address
+  bzero((char *) &serveraddr, sizeof(serveraddr)); // places zero-valued bytes in serveraddr
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_port = htons((unsigned short)port_number);
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // bind the server socket with port number
+  if (bind(parentfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
+    fprintf(stderr, "ERROR on binding");
+
+  // server socket is ready to accept connection requests, 5 requests can be queued up
+  if (listen(parentfd, 5) < 0)
+    fprintf(stderr, "ERROR on listen");
+
+  clientlen = sizeof(clientaddr);
+  while(1) {
+    // server socket is waiting for client requests
+    childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (childfd < 0)
+      fprintf(stderr, "ERROR on accept");
+
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL)
+      fprintf(stderr, "ERROR on gethostbyaddr");
+    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL)
+      fprintf(stderr, "ERROR on inet_ntoa\n");
+    printf("server established connection with %s (%s)\n", hostp->h_name, hostaddrp);
+
+    // read input string from client
+    bzero(buf, BUFSIZE);
+    n = read(childfd, buf, BUFSIZE);
+    if (n < 0)
+      fprintf(stderr, "ERROR reading from socket");
+    printf("server received %d bytes: %s", n, buf);
+
+    // write back to the client
+    n = write(childfd, output_msg, strlen(output_msg));
+    if (n < 0)
+      fprintf(stderr, "%s\n", "ERROR writing to socket");
+
+   close(childfd);
+  }
+}
